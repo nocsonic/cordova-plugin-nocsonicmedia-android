f3c267beeb2476003318b2317fc15724f01cf9a2
Will Streeter <w.streeter@mevio.com>
2015-09-21 22:21:14 -0700

    initial upload for Android noconsic plugin

---
+++ src/android/FileHelper.java
@@ -0,0 +1,21 @@
+
+package org.nocsonic.nocsonicmedia;
+
+import android.net.Uri;
+
+public class FileHelper {
+
+    /**
+     * Removes the "file://" prefix from the given URI string, if applicable.
+     * If the given URI string doesn't have a "file://" prefix, it is returned unchanged.
+     *
+     * @param uriString the URI string to operate on
+     * @return a path without the "file://" prefix
+     */
+    public static String stripFileProtocol(String uriString) {
+        if (uriString.startsWith("file://")) {
+            return Uri.parse(uriString).getPath();
+        }
+        return uriString;
+    }
+}
